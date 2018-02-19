c346052f73b9cc971390bbca2ed91655493fc0f3
David Porter <davidbrianporter@gmail.com>
2018-02-19 16:28:22 -0500

    committing my business website to github

---
+++ public_html/public_html/assets/config.js
@@ -0,0 +1,40 @@
+// cpanel - site_templates/vcard_dev/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        phone: "216 622-5203",
+        fax: "",
+        email: "davidbrianporter@gmail.com",
+        address: "Hudson Ohio",
+        social: [
+            
+            
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+        accent: ""
+    },
+    slides: [
+        {
+            prefix: "",
+            title: "David Porter",
+            subtitle: "Expert Programmer and Creative",
+            type: 'vcard',
+            backgroundImage: "",
+            backgroundColor: "",
+            color: "",
+            buttonText: "",
+            buttonLink: "",
+            biography: "",
+            portraitImage: ""
+        }
+    ]
+};
